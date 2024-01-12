06dd86fbf4f83eb4be6faa28d6ce423a94a776d0
sievl <poxcia@outlook.com>
2024-01-12 22:31:19 +0700

    update

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -4,7 +4,7 @@ const Documentation = () => {
   return (
     <main className="flex flex-col lg:grid lg:grid-cols-4 w-full h-auto lg:h-[480px] items-center justify-center gap-8 lg:gap-16 p-6 lg:p-16">
       <div className="flex flex-col w-full">
-        <p className="text-black text-4xl font-semibold tracking-tight w-full text-start mt-0">
+        <p className="text-black text-2xl font-semibold tracking-tight w-full text-start mt-0">
           Documentation
         </p>
         <p className="text-black/80 text-lg font-medium tracking-tight w-full text-start mt-4 lg:mt-8">
