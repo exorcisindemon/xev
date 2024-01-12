518635d6cc69f25d173db2d471e40148665e7dde
sievl <poxcia@outlook.com>
2024-01-12 19:31:40 +0700

    update

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -2,14 +2,37 @@ import React from "react";
 
 const Documentation = () => {
   return (
-    <main className="flex flex-col w-full h-auto lg:h-[480px] items-start justify-center p-8 lg:p-16">
-      <p className="text-black text-4xl font-semibold tracking-tight w-full text-start mt-0">
-        Documentation
-      </p>
-      <p className="text-black/80 text-lg font-medium tracking-tight w-full lg:w-[480px] text-start mt-8">
-        Welcome to our &rsquo;Documentations&rsquo; hub – your go-to resource
-        for unlocking the full potential of your landing page creation journey.
-      </p>
+    <main className="flex flex-col lg:grid lg:grid-cols-3 w-full h-auto lg:h-[480px] items-center justify-center gap-8 lg:gap-16 p-8 lg:p-16">
+      <div className="flex flex-col w-full">
+        <p className="text-black text-4xl font-semibold tracking-tight w-full text-start mt-0">
+          Documentation
+        </p>
+        <p className="text-black/80 text-lg font-medium tracking-tight w-full text-start mt-4 lg:mt-8">
+          Welcome to our &rsquo;Documentations&rsquo; hub – your go-to resource
+          for unlocking the full potential of your landing page creation
+          journey.
+        </p>
+      </div>
+      <div className="flex flex-col w-full">
+        <p className="text-black text-4xl font-semibold tracking-tight w-full text-start mt-0">
+          Documentation
+        </p>
+        <p className="text-black/80 text-lg font-medium tracking-tight w-full text-start mt-4 lg:mt-8">
+          Welcome to our &rsquo;Documentations&rsquo; hub – your go-to resource
+          for unlocking the full potential of your landing page creation
+          journey.
+        </p>
+      </div>
+      <div className="flex flex-col w-full">
+        <p className="text-black text-4xl font-semibold tracking-tight w-full text-start mt-0">
+          Documentation
+        </p>
+        <p className="text-black/80 text-lg font-medium tracking-tight w-full text-start mt-4 lg:mt-8">
+          Welcome to our &rsquo;Documentations&rsquo; hub – your go-to resource
+          for unlocking the full potential of your landing page creation
+          journey.
+        </p>
+      </div>
     </main>
   );
 };
