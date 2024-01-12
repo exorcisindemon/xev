f559c9d1a4a0be94c552da87d2c011129721f794
sievl <poxcia@outlook.com>
2024-01-12 22:22:10 +0700

    update

--- components/header.jsx
+++ components/header.jsx
@@ -13,26 +13,13 @@ const Hero = () => {
     <header
       className={`${css.header} flex flex-col w-full h-screen lg:h-[720px] items-center justify-center p-6 lg:p-16`}
     >
-      <div className="flex flex-col lg:flex-row items-center gap-4 w-auto">
-        <button className="transition-all bg-green-600/20 hover:bg-green-600/40 ring-1 ring-green-600 flex flex-row gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
-          <Image src={GitPullRequest} width="16" height="16" />
-          <p className="text-green-200 text-sm font-medium tracking-tight w-full text-start lg:text-center">
-            Discover the latest changes! -{" "}
-            <span className="font-bold">03d30f</span>
-          </p>
-        </button>
-        <div className="flex flex-row gap-4 items-center">
-          <button className="transition-all bg-slate-600/20 hover:bg-slate-600/40 ring-1 ring-slate-600 flex flex-row gap-2 items-center w-fit p-2 mt-0 rounded-full">
-            <Image src={Apple} width="16" height="16" />
-          </button>
-          <button className="transition-all bg-stone-600/20 hover:bg-stone-600/40 ring-1 ring-stone-600 flex flex-row gap-2 items-center w-fit p-2 mt-0 rounded-full">
-            <Image src={GitHub} width="16" height="16" />
-          </button>
-          <button className="transition-all bg-red-600/20 hover:bg-red-600/40 ring-1 ring-red-600 flex flex-row gap-2 items-center w-fit p-2 mt-0 rounded-full">
-            <Image src={Instagram} width="16" height="16" />
-          </button>
-        </div>
-      </div>
+      <button className="transition-all bg-green-600/20 hover:bg-green-600/40 ring-1 ring-green-600 flex flex-row gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
+        <Image src={GitPullRequest} width="16" height="16" />
+        <p className="text-green-200 text-sm font-semibold tracking-tight w-full text-start lg:text-center">
+          Discover the latest changes! -{" "}
+          <span className="font-bold">03d30f</span>
+        </p>
+      </button>
       <h1 className="text-white text-4xl font-semibold tracking-tight w-full text-start lg:text-center mt-16">
         Introducing, <span className="text-blue-400">XEV</span> – Your Easy
         Landing Page Builder!
@@ -43,6 +30,26 @@ const Hero = () => {
         business owner, startup, or just want to showcase something awesome, XEV
         has got your back.
       </h2>
+      <div className="flex flex-row gap-4 items-center w-full lg:w-auto mt-16">
+        <button className="transition-all bg-stone-600/20 hover:bg-stone-600/40 ring-1 ring-stone-600 flex flex-row gap-2 items-center w-fit p-2 mt-0 rounded-full">
+          <Image src={Apple} width="28" height="28" />
+          <p className="text-green-200 text-sm font-semibold tracking-tight w-full text-start lg:text-center">
+          Apple
+        </p>
+        </button>
+        <button className="transition-all bg-slate-600/20 hover:bg-slate-600/40 ring-1 ring-slate-600 flex flex-row gap-2 items-center w-fit p-2 mt-0 rounded-full">
+          <Image src={GitHub} width="28" height="28" />
+          <p className="text-green-200 text-sm font-semibold tracking-tight w-full text-start lg:text-center">
+          GitHub
+        </p>
+        </button>
+        <button className="transition-all bg-red-600/20 hover:bg-red-600/40 ring-1 ring-red-600 flex flex-row gap-2 items-center w-fit p-2 mt-0 rounded-full">
+          <Image src={Instagram} width="28" height="28" />
+          <p className="text-green-200 text-sm font-semibold tracking-tight w-full text-start lg:text-center">
+          Instagram
+        </p>
+        </button>
+      </div>
     </header>
   );
 };
