0095910cb4ed22a389b28010a6be5aa338917f2a
sievl <poxcia@outlook.com>
2024-01-13 03:38:41 +0700

    update

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -1,4 +1,8 @@
-import css from "../shared/documentation/documentation.module.css"
+import Image from "next/image";
+import css from "../shared/documentation/documentation.module.css";
+
+const { default: BookOpen } = require("../shared/icons/book-open-line.svg");
+const { default: ArrowRight } = require("../shared/icons/arrow-right-line.svg");
 
 const Documentation = () => {
   return (
@@ -6,9 +10,12 @@ const Documentation = () => {
       className={`${css.documentation} flex flex-col lg:grid lg:grid-cols-4 w-full h-auto lg:h-[480px] items-center justify-center gap-8 lg:gap-16 p-6 lg:p-16`}
     >
       <div className="flex flex-col w-full">
-        <p className="text-black text-2xl font-semibold tracking-tight w-full text-start mt-0">
-          Documentation
-        </p>
+        <div className="flex flex-row gap-2 lg:gap-4 items-center w-full">
+          <Image src={BookOpen} width="20" height="20" />
+          <p className="text-black text-2xl font-semibold tracking-tight w-auto text-start mt-0">
+            Documentation
+          </p>
+        </div>
         <p className="text-black/80 text-lg font-medium tracking-tight w-full text-start mt-4 lg:mt-8">
           Welcome to our &rsquo;Documentations&rsquo; hub – your go-to resource
           for unlocking the full potential of your landing page creation
@@ -32,9 +39,12 @@ const Documentation = () => {
         </p>
       </div>
       <div className="flex flex-col w-full">
-        <p className="text-black text-2xl font-semibold tracking-tight w-full text-start mt-0">
-          See Full Documentation
-        </p>
+        <div className="flex flex-row gap-2 lg:gap-4 items-center w-full">
+          <p className="text-black text-2xl font-semibold tracking-tight w-auto text-start mt-0">
+            See Full Documentation
+          </p>
+          <Image src={ArrowRight} width="20" height="20" />
+        </div>
         <p className="text-black/80 text-lg font-medium tracking-tight w-full text-start mt-4 lg:mt-8">
           Welcome to our &rsquo;Documentations&rsquo; hub – your go-to resource
           for unlocking the full potential of your landing page creation
