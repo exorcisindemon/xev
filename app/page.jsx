3239e24be855616224144a74cb1061adbd46c5cf
sievl <poxcia@outlook.com>
2024-01-15 20:41:54 +0700

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-import { LikeProvider } from "@/shared/hooks/likeContext";
-
 const {
   default: ScreenLayout,
 } = require("../components/layouts/screen-layout");
@@ -9,12 +7,10 @@ const { default: Documentation } = require("../components/documentation");
 export default function App() {
   return (
     <ScreenLayout>
-      <LikeProvider>
-        <div className="relative top-auto lg:top-[72px]">
-          <Header />
-          <Documentation />
-        </div>
-      </LikeProvider>
+      <div className="relative top-auto lg:top-[72px]">
+        <Header />
+        <Documentation />
+      </div>
     </ScreenLayout>
   );
 }
--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,10 @@
-"use client";
-
 import Image from "next/image";
 import css from "../shared/css/header.module.css";
-
-import { useLikeContext } from "@/shared/hooks/likeContext";
 
 const { default: Command } = require("../shared/icons/command-line.svg");
 const { default: ThumbUp } = require("../shared/icons/thumb-up-line.svg");
 
-const Header = () => {
-  const { likeCount, userLiked, likeHandler } = useLikeContext();
+const Hero = () => {
   return (
     <header
       className={`${css.header} flex flex-col w-full h-screen lg:h-[720px] items-center justify-center p-4 lg:p-16`}
@@ -22,14 +17,10 @@ const Header = () => {
             <span className="font-bold">03d30f</span>
           </p>
         </button>
-        <button
-          onClick={likeHandler}
-          disabled={userLiked}
-          className="transition-all bg-amber-600/20 hover:bg-amber-600/40 ring-1 ring-amber-600 flex flex-row gap-2 items-center w-fit px-4 py-2 mt-0 rounded-full"
-        >
+        <button className="transition-all bg-amber-600/20 hover:bg-amber-600/40 ring-1 ring-amber-600 flex flex-row gap-2 items-center w-fit px-4 py-2 mt-0 rounded-full">
           <Image src={ThumbUp} width="16" height="16" />
           <p className="text-amber-800 text-sm font-medium tracking-tight w-full text-start lg:text-center">
-            {likeCount}
+            0
           </p>
         </button>
       </div>
@@ -47,4 +38,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default Hero;
