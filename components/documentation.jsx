41f2ecf69b1e511e74e69e7179ca93d562bf6097
sievl <poxcia@outlook.com>
2024-01-15 18:27:33 +0700

    update

--- components/documentation.jsx
+++ components/documentation.jsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import css from "../shared/css/documentation.module.css";
 
 const { default: BookOpen } = require("../shared/icons/book-open-line.svg");
-const { default: ArrowRight } = require("../shared/icons/arrow-right-line.svg");
+const { default: ArrowRightGreen } = require("../shared/icons/arrow-right-line-green.svg");
+const { default: ArrowRightBlue } = require("../shared/icons/arrow-right-line-blue.svg");
+const { default: ArrowRightPurple } = require("../shared/icons/arrow-right-line-purple.svg");
 const {
   default: ArrowRightUp,
 } = require("../shared/icons/arrow-right-up-line.svg");
@@ -26,7 +28,7 @@ const Documentation = () => {
         </p>
       </div>
       <div className="flex flex-col lg:grid lg:grid-cols-4 gap-8 lg:gap-16 items-center w-full h-full">
-        <div className="transition-all bg-white/60 ring-0 lg:ring-1 lg:ring-black/20 flex flex-col w-full h-full justify-between gap-16 p-4 lg:p-8 rounded-lg">
+        <div className="transition-all bg-white/60 lg:hover:bg-green-400/60 ring-1 ring-green-600 lg:ring-black/20 hover:lg:ring-green-600 flex flex-col w-full h-full justify-between gap-16 p-4 lg:p-8 rounded-lg">
           <div className="flex flex-col items-start gap-4">
             <p className="text-black text-lg font-semibold tracking-tight w-full text-start mt-0">
               Introduction
@@ -36,13 +38,13 @@ const Documentation = () => {
             </p>
           </div>
           <button className="transition-all bg-green-600/20 hover:bg-green-600/40 ring-1 ring-green-600 flex flex-row-reverse gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
-            <Image src={ArrowRight} width="16" height="16" />
+            <Image src={ArrowRightGreen} width="16" height="16" />
             <p className="text-green-800 text-sm font-medium tracking-tight w-full text-start lg:text-center">
-              Read <span className="font-bold">Getting Started</span>
+              Read <span className="font-bold">Introduction</span>
             </p>
           </button>
         </div>
-        <div className="transition-all bg-white/60 ring-0 lg:ring-1 lg:ring-black/20 flex flex-col w-full h-full justify-between gap-16 p-4 lg:p-8 rounded-lg">
+        <div className="transition-all bg-white/60 lg:hover:bg-blue-400/60 ring-1 ring-blue-600 lg:ring-black/20 hover:lg:ring-blue-600 flex flex-col w-full h-full justify-between gap-16 p-4 lg:p-8 rounded-lg">
           <div className="flex flex-col items-start gap-4">
             <p className="text-black text-lg font-semibold tracking-tight w-full text-start mt-0">
               Getting Started
@@ -51,14 +53,14 @@ const Documentation = () => {
               &undefined
             </p>
           </div>
-          <button className="transition-all bg-green-600/20 hover:bg-green-600/40 ring-1 ring-green-600 flex flex-row-reverse gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
-            <Image src={ArrowRight} width="16" height="16" />
-            <p className="text-green-800 text-sm font-medium tracking-tight w-full text-start lg:text-center">
+          <button className="transition-all bg-blue-600/20 hover:bg-blue-600/40 ring-1 ring-blue-600 flex flex-row-reverse gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
+            <Image src={ArrowRightBlue} width="16" height="16" />
+            <p className="text-blue-800 text-sm font-medium tracking-tight w-full text-start lg:text-center">
               Read <span className="font-bold">Getting Started</span>
             </p>
           </button>
         </div>
-        <div className="transition-all bg-white/60 ring-0 lg:ring-1 lg:ring-black/20 flex flex-col w-full h-full justify-between gap-16 p-4 lg:p-8 rounded-lg">
+        <div className="transition-all bg-white/60 lg:hover:bg-purple-400/60 ring-1 ring-purple-600 lg:ring-black/20 hover:lg:ring-purple-600 flex flex-col w-full h-full justify-between gap-16 p-4 lg:p-8 rounded-lg">
           <div className="flex flex-col items-start gap-4">
             <p className="text-black text-lg font-semibold tracking-tight w-full text-start mt-0">
               Knowing Specification
@@ -67,9 +69,9 @@ const Documentation = () => {
               &undefined
             </p>
           </div>
-          <button className="transition-all bg-green-600/20 hover:bg-green-600/40 ring-1 ring-green-600 flex flex-row-reverse gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
-            <Image src={ArrowRight} width="16" height="16" />
-            <p className="text-green-800 text-sm font-medium tracking-tight w-full text-start lg:text-center">
+          <button className="transition-all bg-purple-600/20 hover:bg-purple-600/40 ring-1 ring-purple-600 flex flex-row-reverse gap-2 items-center w-fit px-8 py-2 mt-0 rounded-full">
+            <Image src={ArrowRightPurple} width="16" height="16" />
+            <p className="text-purple-800 text-sm font-medium tracking-tight w-full text-start lg:text-center">
               Read <span className="font-bold">Knowing Specification</span>
             </p>
           </button>
